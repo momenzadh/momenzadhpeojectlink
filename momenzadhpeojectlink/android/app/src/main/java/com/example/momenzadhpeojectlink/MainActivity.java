d61928e456d75e886c8d0ba3347e051bd0849810
momenzadh <129141718+momenzadh@users.noreply.github.com>
2023-06-11 15:52:12 +0430

    this is the commit
    the main of commit

---
+++ momenzadhpeojectlink/android/app/src/main/java/com/example/momenzadhpeojectlink/MainActivity.java
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+package com.example.momenzadhpeojectlink;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
+=======
+package com.example.momenzadhpeojectlink;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
+>>>>>>> origin/main
