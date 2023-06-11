f214e85c1525087040777001489e0611c7797856
momenzadh <mohammadnazemmomenzadh@gmail.com>
2023-06-11 17:45:03 +0430

    the next commit

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,3 +1,8 @@
-class LightThemeColors{
+import 'dart:ui';
 
+class LightThemeColors{
+  static const primaryColor = Color(0xFF217cF3);
+  static const secondaryColor = Color(0xff262A35);
+  static const primaryTextColor = Color(0xff262A35);
+  static const secondaryTextColor = Color(0xff83B6BE);
 }
